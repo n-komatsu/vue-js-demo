59c6e2255f42e6e82b98f3309106be553280148d
n-komatsu <komatsu@gizumo-inc.jp>
2019-02-10 23:18:26 +0900

    ソート機能付きテーブル実装

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,35 @@
 <template>
   <div>
     <h1>Hello Vue</h1>
+    <form id="search">
+      <input name="query" v-model="searchQuery">
+    </form>
+    <my-grid 
+      :data="gridData"
+      :columns="gridColumns"
+      :filter-key="searchQuery"
+    ></my-grid>
   </div>
 </template>
+
+<script>
+import MyGrid from 'Components/Grid';
+
+export default {
+  data: () => {
+    return {
+      searchQuery: '',
+      gridColumns: ['name', 'power'],
+      gridData: [
+        { name: 'Chuck Norris', power: Infinity },
+        { name: 'Bruce Lee', power: 9000 },
+        { name: 'Jackie Chan', power: 7000 },
+        { name: 'Jet Li', power: 8000 }
+      ],
+    }
+  },
+  components: {
+    MyGrid,
+  },
+}
+</script>
