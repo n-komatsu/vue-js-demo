514cfd66a46645a51199cc447b31606b6afad26c
n-komatsu <komatsu@gizumo-inc.jp>
2019-02-13 14:45:36 +0900

    modal実装

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,33 @@
 <template>
   <div>
     <h1>Hello Vue</h1>
+    <button 
+    id="show-modal"
+    @click="showModal = true"
+    >
+      Show Modal
+    </button>
+    <my-modal 
+      v-if="showModal"
+      @close="showModal = false"
+    >
+      <h3 slot="header">custom header</h3>
+    </my-modal>
   </div>
 </template>
+
+<script>
+import MyModal from 'Components/Modal';
+
+export default {
+  data() {
+    return {
+      showModal: false,
+    }
+  },
+  components: {
+    MyModal
+  },
+}
+</script>
+
